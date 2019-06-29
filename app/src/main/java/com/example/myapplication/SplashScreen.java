99093c0947120a4f299aaa7c8b338246a4f81811
alages97 <e0309551@u.nus.edu>
2019-06-29 18:14:23 +0800

    global

--- app/src/main/java/com/example/myapplication/SplashScreen.java
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -15,19 +15,35 @@ import java.io.InputStream;
 
 public class SplashScreen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
+    Global global;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this,Carousel.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+        global = new Global();
+        if (global.getFirstTime()) {
+            global.setSecondTime();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(SplashScreen.this, Carousel.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+
+        else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
 
     }
 }
