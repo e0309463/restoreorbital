256144c78250c251e4100dec120ccf62de376367
Jess Teo <e0309463@u.nus.edu.sg>
2019-07-01 22:32:15 +0800

    Fragment design

--- app/src/main/java/com/example/myapplication/FoodFragment.java
+++ app/src/main/java/com/example/myapplication/FoodFragment.java
@@ -3,10 +3,15 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 
 /**
@@ -15,17 +20,44 @@ import android.widget.ImageButton;
 public class FoodFragment extends Fragment {
 
     private ImageButton btnBack;
+    private TextView info;
+    private TextView billsExpenditureNumber;
+    private TextView billsBudgetNumber;
+    private TextView resultTv;
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        btnBack = (ImageButton)view.findViewById(R.id.btnBack);
+        btnBack = (ImageButton) view.findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
             }
         });
+
+        info = (TextView) view.findViewById(R.id.tvbillsInfo);
+        info.setMovementMethod(new ScrollingMovementMethod());
+        billsBudgetNumber = (TextView)view.findViewById(R.id.tvBillBudgetNumber);
+        billsExpenditureNumber = (TextView)view.findViewById(R.id.tvBillExpenditureNumber);
+        info = (TextView)view.findViewById(R.id.tvbillsInfo);
+        resultTv = (TextView)view.findViewById(R.id.tvAnalysis);
+        float billsExpend = SecondActivitymodded.getBillsExpend();
+        String BE = "$" + billsExpend + "";
+        billsExpenditureNumber.setText(BE);
+        float billsBudget = 40;
+        String BB ="$" + billsBudget + "";
+        billsBudgetNumber.setText(BB);
+        String result = "";
+        DecimalFormat df = new DecimalFormat("##.##");
+        df.setRoundingMode(RoundingMode.DOWN);
+        if (billsExpend < billsBudget) {
+            result = "You still have "  + df.format((1 - billsExpend/billsBudget)*100 )+ "% of your budget to spend!";
+        } else if (billsExpend > billsBudget){
+            result = "You have exceeded your budget by " + df.format(((billsExpend/billsBudget)-1) * 100) + "%!";
+        } else {
+            result = "You have used up exactly all your budget!";
+        }
+
     }
 
 
---
+++ app/src/main/java/com/example/myapplication/Oauth.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class Oauth extends AppCompatActivity {
+    private String clientId = "dd35d1dd-1c3a-4233-b25a-6ee6c5e9a70c";
+    private String clientSecret = "57ac89db-ea47-4fc4-a122-0295063fd211";
+    private String redirectUri = "https://www.restore6363.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oauth);
+
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com/sandbox/api/sg/v1/oauth/authorize" + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=Read&response_type=code&state=0399"));
+//        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Uri uri = getIntent().getData();
+
+       if (uri != null && uri.toString().startsWith(redirectUri)){
+//            String code = uri.getQueryParameter("code");
+//
+//
+//
+            Toast.makeText(this,"yay!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
